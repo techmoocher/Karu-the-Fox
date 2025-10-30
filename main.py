d32d4cecbcf74759f4fd62e1d90957fd08e767f0
Benjamin Nguyen <92527665+techmoocher@users.noreply.github.com>
2025-10-30 18:41:09 +0700

    main.py: Removing unnecessary imports

--- main.py
+++ main.py
@@ -1,19 +1,6 @@
 import sys
-import os
-import json
 import dotenv
-from pathlib import Path
-from random import choice, random, randint
-from datetime import datetime
-from PySide6.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout, QDialog,
-                               QPushButton, QHBoxLayout, QRadioButton, QButtonGroup, QMenu,
-                               QSystemTrayIcon, QListWidget, QSlider, QStyle, QListWidgetItem,
-                               QGraphicsDropShadowEffect, QFrame)
-from PySide6.QtGui import QPixmap, QMovie, QAction, QIcon, QCursor, QColor
-from PySide6.QtCore import Qt, QTimer, QUrl, QSize, QPoint
-from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
-
-import sys
+from PySide6.QtWidgets import QApplication
 from PySide6.QtWidgets import QApplication
 from src.desktop_pet import DesktopPet
 
